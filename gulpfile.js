322a9f8083aa04e0eee90b803a9376c576c7a972
cusoq <cusoq@mail.ru>
2021-07-25 17:09:34 +0300

    Add the adaptive header layout

--- gulpfile.js
+++ gulpfile.js
@@ -67,7 +67,7 @@ gulp.task("webp", function () {
 });
 
 gulp.task("sprite", function () {
-  return gulp.src("source/img/{icon-*,htmlacademy*}.svg")
+  return gulp.src("source/img/*.svg")
     .pipe(svgstore({inlineSvg: true}))
     .pipe(rename("sprite_auto.svg"))
     .pipe(gulp.dest("build/img"));
@@ -97,5 +97,5 @@ gulp.task("clean", function () {
   return del("build");
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+gulp.task("build", gulp.series("clean", "copy", "css", "webp", "sprite", "html"));
 gulp.task("start", gulp.series("build", "server"));
