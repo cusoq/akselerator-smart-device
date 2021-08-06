c61861605a0c3907167caa66ca5db5fead496add
cusoq <cusoq@mail.ru>
2021-08-06 22:07:02 +0300

    Set focus trap

--- build/js/script.js
+++ build/js/script.js
@@ -1,41 +1,15 @@
 'use strict';
 
-var ESC_KEYCODE = 27;
-var ENTER_KEYCODE = 13;
-var pageElement = document.querySelector('body');
-var popupOverlayElement = document.querySelector('.modal-overlay');
-var modalBlockElement = document.querySelector('.modal');
 var popupNameInputElement = document.getElementById('modal-name-input');
 var popupTelInputElement = document.getElementById('modal-tel-input');
 var popupTextareaElement = document.getElementById('modal-textarea');
 var feedbackNameInputElement = document.getElementById('feedback-name-input');
 var feedbackTelInputElement = document.getElementById('feedback-tel-input');
 var feedbackTextareaElement = document.getElementById('feedback-textarea');
-var crossButtonElement = modalBlockElement.querySelector('.modal__close-button');
-var modalForm = modalBlockElement.querySelector('.modal__form');
+var modalForm = document.querySelector('.modal__form');
 var feedbackForm = document.querySelector('.feedback__form');
-var recallButtonElement = document.querySelector('.header__recall-link');
 var anchorsElements = document.querySelectorAll('a[href*="#"]');
 var requiredInputElements = document.querySelectorAll('input:required');
-
-// установка фокуса по умолчанию
-var getFocus = function () {
-  if (popupOverlayElement) {
-    popupNameInputElement.focus();
-  }
-};
-
-getFocus();
-
-// открытие модального окна
-var showPopup = function () {
-  if (popupOverlayElement || modalBlockElement || modalForm) {
-    popupOverlayElement.classList.remove('visually-hidden');
-    modalBlockElement.classList.remove('visually-hidden');
-    modalForm.addEventListener('submit', onSubmit);
-    // pageElement.classList.add('non-scroll');
-  }
-};
 
 // валидация заполнения полей и запись в localStorage
 
@@ -50,17 +24,6 @@ var setLocalStorage = function () {
   }
   modalForm.submit();
   feedbackForm.submit();
-  closePopup();
-};
-
-
-// закрытие модального окна
-var closePopup = function () {
-  if (popupOverlayElement || modalBlockElement) {
-    popupOverlayElement.classList.add('visually-hidden');
-    modalBlockElement.classList.add('visually-hidden');
-    pageElement.classList.remove('non-scroll');
-  }
 };
 
 // Плавная прокрутка для якорных ссылок
@@ -94,46 +57,11 @@ var onChangeInputValue = function (evt) {
   checkValidity(evt);
 };
 
-var onClickOpener = function (evt) {
-  evt.preventDefault();
-  showPopup();
-  getFocus();
-  popupOverlayElement.addEventListener('click', onClickCloser);
-  crossButtonElement.addEventListener('click', onClickCloser);
-};
-
-var onClickCloser = function (evt) {
-  evt.preventDefault();
-  if (evt.target !== modalBlockElement) {
-    closePopup();
-  }
-};
-
-var onEnterOpener = function (evt) {
-  if (evt.keyCode === ENTER_KEYCODE) {
-    evt.preventDefault();
-    showPopup();
-    getFocus();
-  }
-};
-
-var onEscCloser = function (evt) {
-  if (evt.keyCode === ESC_KEYCODE) {
-    evt.preventDefault();
-    document.removeEventListener('keydown', onEscCloser);
-    closePopup();
-  }
-};
-
 var onSubmit = function (evt) {
   evt.preventDefault();
   setLocalStorage();
 };
 
-recallButtonElement.addEventListener('click', onClickOpener);
-recallButtonElement.addEventListener('keydown', onEnterOpener);
-
-document.addEventListener('keydown', onEscCloser);
 requiredInputElements.forEach(function (element) {
   element.addEventListener('change', onChangeInputValue);
 });
